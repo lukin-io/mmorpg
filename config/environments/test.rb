6d1f221b6372629258e022eb57d2abb0257d9d07
Max kerby <max.l@wigiwork.com>
2026-05-10 00:50:29 +0300

    Updates+

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = "test-secret-key-base-for-mmorpg-ci-and-local-test-suite-only"
+
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
 
